23655b02523604583fdcf08e4e85b04874ebbb40
Sergey Tarasov <tarasovsergeyrom@gmail.com>
2022-01-15 01:10:19 +0200

    API before renders
    дописал API. начал писать рендер, нужны hbs

---
+++ src/js/dev-1/api.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.API_KEY = '90cb713cbad21b579532fb5c59ca1f23';
+    this.BASE_URL = `https://api.themoviedb.org/3`;
+      
+  }
+  // Запрос популярных фильмов
+  async fetchMovieTrends() {
+    const url = `${this.BASE_URL}/movie/popular?api_key=${this.API_KEY}&page=${this.page}`;
+      return await fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              console.log(data);
+              this.incrementPage();
+              return data;
+          });
+  }
+  // Запрос с использованием поисковой строки
+  async fetchSearchMovies() {
+    const url = `${this.BASE_URL}/search/movie?api_key=${this.API_KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+      return await fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              this.incrementPage();
+              return data;
+          }); 
+  }
+
+  //Запрос для полной информации о фильме
+  async fetchFullInfo(id) {
+    const url = `${this.BASE_URL}/movie/${id}/videos?api_key=${this.APIKEY}&language=en-US`;
+      return await fetch(url).then(response => response.json()); 
+  }
+  
+  //Запрос для жанров
+  async fetchMovieGenre() {
+    const url = `${this.BASE_URL}/genre/movie/list?api_key=${this.API_KEY}&language=en-US`;
+      return await fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              return data.genres;
+          }); 
+  }
+  
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  set pageNumber(pageNumber) {
+    this.page = pageNumber;
+  }
+  get pageNumber() {
+    return this.page;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/js/dev-1/dev-1-main.js
+++ src/js/dev-1/dev-1-main.js
@@ -1,1 +1,26 @@
-console.log(`1`)
+console.log(1);
+//import movieTpl from '../../templates/film-card-main.hbs'
+//import movieLibraryTpl from '../../templates/film-card-library.hbs'
+
+import ApiService from "./api";
+
+const apiService = new ApiService();
+
+apiService.fetchMovieTrends();
+
+/*
+// Для рендера популярных фильмов
+function renderTrends(data) {
+  const markup = movieTpl(data);
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+// Рендер библиотеки
+function renderLibrary(data) {
+  const markup = movieLibraryTpl(data);
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+export { renderTrends, renderLibrary };*/
