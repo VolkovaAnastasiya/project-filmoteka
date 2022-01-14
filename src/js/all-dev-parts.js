f7426a42747ffc20d3efde00afe3f1855ed1fc13
PiekharievaK <pechareva@gmail.com>
2022-01-14 02:55:54 +0200

    добавляет папку с основным  js файлом для каждого разработчика. и подключает.

---
+++ src/js/all-dev-parts.js
@@ -0,0 +1,8 @@
+import './dev-1/dev-1-main.js';
+import './dev-2/dev-2-main.js';
+import './dev-3/dev-3-main.js';
+import './dev-4/dev-4-main.js';
+import './dev-5/dev-5-main.js';
+import './dev-6/dev-6-main.js';
+import './dev-7/dev-7-main.js';
+const API_KEY = `718b7347396ac1052bb7bdc76b11dbfa`;
