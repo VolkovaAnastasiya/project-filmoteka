e853b68afa8719d5e318da5c11f62638246ddada
Alexandr Sorokin <sanek1678@gmail.com>
2022-01-15 14:27:52 +0200

    Update dev-2-main.js

--- src/js/dev-2/dev-2-main.js
+++ src/js/dev-2/dev-2-main.js
@@ -1,9 +1,9 @@
 console.log(`2`)
-const searchForm = document.querySelector('.search-form');
-const searchField = document.querySelector('.search-form_input').value;
+// const searchForm = document.querySelector('.search-form');
+// const searchField = document.querySelector('.search-form_input').value;
 
-searchForm.addEventListener('submit', renderSearch);
+// searchForm.addEventListener('submit', renderSearch);
 
-function renderSearch(event, data) {
-    event.preventDefault();
-}
+// function renderSearch(event, data) {
+//     event.preventDefault();
+// }
