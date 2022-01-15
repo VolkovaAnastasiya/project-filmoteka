2364c5b431bbf13a976277ee4a98599e68a2d3e8
Alexandr Sorokin <sanek1678@gmail.com>
2022-01-15 19:59:27 +0200

    Films added by request
    Функционал поиска фильма по запросу

--- src/js/dev-2/dev-2-main.js
+++ src/js/dev-2/dev-2-main.js
@@ -1,1 +1,29 @@
 console.log(`2`)
+import movieTpl from '../../templates/film-card-main.hbs'
+ import ApiService from '../dev-1/api.js';
+ const apiService = new ApiService();
+
+const searchForm = document.querySelector('.search-form');
+const gallery = document.querySelector('.cards-gallery__list');
+
+searchForm.addEventListener('submit', renderSearch);
+
+
+function renderSearch(event) {
+    event.preventDefault();
+
+    const searchFieldValue = document.querySelector('.search-form_input').value;
+    apiService.searchQuery = searchFieldValue;
+    apiService.fetchSearchMovies().then(renderSearchedFilms)
+
+    console.log(searchFieldValue);
+
+    function renderSearchedFilms(data) {
+    const markup = movieTpl(data.results);
+    console.log(data.results);
+        gallery.innerHTML = markup;
+        localStorage.setItem("searchResults",JSON.stringify(data.results));
+    }
+}
+  
+export{ renderSearch}
