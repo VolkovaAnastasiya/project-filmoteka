1715648dcb327a7ce596c4cfdef83f87b81868fd
Sergey Tarasov <tarasovsergeyrom@gmail.com>
2022-01-15 02:18:48 +0200

    Changing

--- src/js/dev-1/dev-1-main.js
+++ src/js/dev-1/dev-1-main.js
@@ -2,9 +2,6 @@ console.log(1);
 //import movieTpl from '../../templates/film-card-main.hbs'
 //import movieLibraryTpl from '../../templates/film-card-library.hbs'
 
-import ApiService from "./api";
-
-const apiService = new ApiService();
 
 apiService.fetchMovieTrends();
 
