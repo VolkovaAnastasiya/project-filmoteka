421bc6696f26153d43d6558a641f1ba195fdae72
Katrusya Panchenko <katrusya.pan4enko@gmail.com>
2022-01-16 23:13:59 +0200

    пофиксила модалку
    отрисовка из локал сторадж  по популярнім фильмам и по ключевому слову, закрытие кнопкой еск

--- src/js/dev-3/dev-3-main.js
+++ src/js/dev-3/dev-3-main.js
@@ -2,19 +2,21 @@ import movieModalTpl from '../../templates/film-details.hbs'
  import ApiService from '../dev-1/api.js';
  const apiService = new ApiService();
 
-const openModal = document.querySelector("[film-modal-open]");
-const closeModal = document.querySelector("[film-modal-close]");
-const modalFilm = document.querySelector("[film-modal]");
+const openModal = document.querySelector("[data-film-modal-open]");
+const closeModal = document.querySelector("[data-film-modal-close]");
+const modalFilm = document.querySelector("[data-film-modal]");
 const backdrop = document.querySelector(".backdrop__film");
 const boxFilm = document.querySelector(".modal__film-detalies");
-// const  galleryItem = document.querySelector(".movie-card")
+
+
+
 
 openModal.addEventListener('click', onOpenModalFilm);
 closeModal.addEventListener('click', onCloseModalFilm);
 window.addEventListener('keydown', closeModalByEsc);
 backdrop.addEventListener('click', closeModalByClick);
 modalFilm.addEventListener('click', closeModalByClick)
-
+// addWatchedBtn.addEventListener('click', )
 //    apiService.fetchMovieTrends().then(data => {
 //   localStorage.setItem('filmInfo', JSON.stringify(data));
 //   return data
@@ -24,35 +26,34 @@ function onOpenModalFilm(event) {
 if (event.target.nodeName !== "IMG") {
     return;
   }
-
-  
-
   const filmId = event.target.parentNode.id;
-// console.log(filmId)
-  // localStorage.setItem('filmInfo', JSON.stringify(currentFilm))
+  
   const filmItem = JSON.parse(localStorage.getItem("filmInfo"));
+  const filmItemSearch = JSON.parse(localStorage.getItem("searchResults"));
 
-  const newList = filmItem.find((elem) => elem.id === Number(filmId) );
+  const newList = filmItem.find((elem) => elem.id === Number(filmId)) || filmItemSearch.find((elem) => elem.id === Number(filmId));
 
-  //  console.log(newList )
+  renderModal(newList)
   
     document.querySelector('body').classList.add('is-overflow')
     backdrop.classList.remove('is-hidden');
   
-renderModal(newList)
-  
     window.addEventListener('keydown', closeModalByEsc);
     backdrop.addEventListener('click', closeModalByClick);
 
- 
-
+//  getLocalStorage()
+// function getLocalStorage(event) {
+  
+// }
 }
+
 
 
 
 function renderModal(data) {
 //  console.log(data);
-   const filmItem = JSON.parse(localStorage.getItem("filmInfo"));
+  const filmItem = JSON.parse(localStorage.getItem("filmInfo"));
+   const filmItemSearch = JSON.parse(localStorage.getItem("searchResults"));
   const markup = movieModalTpl(data);
   boxFilm.innerHTML = markup;
 
@@ -60,6 +61,18 @@ function renderModal(data) {
   console.log(apiService.data)
   
 }
+// function renderModalSearch(data) {
+// //  console.log(data);
+  
+//   const filmItemSearch = JSON.parse(localStorage.getItem("searchResults"));
+//   const markup = movieModalTpl(data);
+//   boxFilm.innerHTML = markup;
+
+//   // const currentFilm = data;
+//   console.log(apiService.data)
+  
+// }
+
 
 function onCloseModalFilm(event) {
 
@@ -78,12 +91,13 @@ function onCloseModalFilm(event) {
 }
   
 function closeModalByClick(e){
- console.log(e.target)
-    if (e.target === modalFilm) {
-      return;
-  }
-     onCloseModalFilm()
+  // console.log(e.target)
+ 
+  if (e.target === backdrop) {
+       onCloseModalFilm()
     backdrop.removeEventListener('click', closeModalByClick);
+  }
+  
 }
 
 
