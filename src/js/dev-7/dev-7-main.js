5a63777cd948b5c0793ed3696194042d4995d400
Alexandr-Yermolov <dirynchik@gmail.com>
2022-01-17 22:43:25 +0200

    js отрисовка второй страници, исправил контейнер 768

--- src/js/dev-7/dev-7-main.js
+++ src/js/dev-7/dev-7-main.js
@@ -1,1 +1,29 @@
 console.log(`7`)
+import refs from './refs';
+
+const { header, btnHome, btnMyLibrary, formSearch, navLibrary } = refs;
+
+
+
+btnHome.addEventListener('click', onBtnLogoHomeClick);
+btnMyLibrary.addEventListener('click', onBtnMyLibraryClick);
+
+function toggleClass(refsRemove, refsAdd, classs) {
+  refsRemove.classList.remove(classs);
+  refsAdd.classList.add(classs);
+}
+
+
+function onBtnLogoHomeClick() {
+  toggleClass(formSearch, navLibrary, 'visually-hidden');
+  toggleClass(btnMyLibrary, btnHome, 'current-link');
+  header.classList.remove('header_library');
+  header.classList.add('header');
+}
+
+function onBtnMyLibraryClick() {
+  toggleClass(navLibrary, formSearch, 'visually-hidden');
+  toggleClass(btnHome, btnMyLibrary, 'current-link');
+  header.classList.add('header_library');
+  header.classList.remove('header');
+}
---
+++ src/js/dev-7/refs.js
@@ -0,0 +1,14 @@
+export default {
+    // --------- header --------
+    header: document.querySelector('.header-js'),
+    btnHome: document.querySelector('.btn-home-js'),
+    btnMyLibrary: document.querySelector('.btn-myLibrary-js'),
+    btnSubmit: document.querySelector('.btn-submit-js'),
+    formSearch: document.querySelector('.form-js'),
+    navLibrary: document.querySelector('.nav-library-js'),
+
+    // ---------- library --------------
+    btnWatched: document.querySelector('.btn-watched-js'),
+    btnQueue: document.querySelector('.btn-queue-js'),
+}
+
