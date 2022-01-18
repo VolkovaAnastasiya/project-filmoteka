75201ae8e7d12230fa639b6a241c4e59551683aa
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2022-01-18 11:06:25 +0200

    Добавляет ключи для рендера библиотеки

--- src/js/all-dev-parts.js
+++ src/js/all-dev-parts.js
@@ -8,24 +8,31 @@ import './dev-7/dev-7-main.js';
 
 import ApiService from './dev-1/api.js';
 import { renderTrends, renderLibrary } from './dev-1/renders.js';
-import { renderModal } from './dev-3/dev-3-main.js'
-import{renderSearch, idToGenre} from './dev-2/dev-2-main.js'
-
+import { renderModal } from './dev-3/dev-3-main.js';
+import { renderSearch, idToGenre } from './dev-2/dev-2-main.js';
 
 const API_KEY = `718b7347396ac1052bb7bdc76b11dbfa`;
 const apiService = new ApiService();
 apiService.fetchMovieGenre().then(saveGenreList);
 
-function saveGenreList (list) {
-  localStorage.setItem('genreList',JSON.stringify(list))
+function saveGenreList(list) {
+  localStorage.setItem('genreList', JSON.stringify(list));
 }
 
-  apiService.fetchMovieTrends().then(data => {
-  localStorage.setItem('filmInfo', JSON.stringify(data));
-  return data
-}).then(renderTrends);
+apiService
+  .fetchMovieTrends()
+  .then(data => {
+    localStorage.setItem('filmInfo', JSON.stringify(data));
+    return data;
+  })
+  .then(renderTrends);
 
-idToGenre(JSON.parse(localStorage.getItem('genreList')), JSON.parse(localStorage.getItem('filmInfo')))
+idToGenre(
+  JSON.parse(localStorage.getItem('genreList')),
+  JSON.parse(localStorage.getItem('filmInfo')),
+);
 
 import './dev-1/pagination.js';
 
+localStorage.setItem('watched', JSON.stringify([]));
+localStorage.setItem('queue', JSON.stringify([]));
