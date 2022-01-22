8359ca5ad9a7289f8afa13d672adadd026479b2d
Yura Dolzhnikov <yuradolzhnikov@gmail.com>
2022-01-22 10:55:51 +0200

    Update dev-5-main.js
    Добавил экспорты для правильной удаления фильмов из библиотеки

--- src/js/dev-5/dev-5-main.js
+++ src/js/dev-5/dev-5-main.js
@@ -1,16 +1,16 @@
-import ApiService from "../dev-1/api.js";
-import { renderTrends, renderLibrary } from "../dev-1/renders";
-import movieTpl from "../../templates/film-card-library.hbs";
-import { idToGenre } from "../dev-2/dev-2-main";
-import pagination from "../dev-1/pagination";
+import ApiService from '../dev-1/api.js';
+import { renderTrends, renderLibrary } from '../dev-1/renders';
+import movieTpl from '../../templates/film-card-library.hbs';
+import { idToGenre } from '../dev-2/dev-2-main';
+import pagination from '../dev-1/pagination';
 
 ////////////////// Когда жмешь на кнопку Home, то рисуется галлерея
 
 const apiService = new ApiService();
 
-const homeBtn = document.querySelector(".btn-home-js");
+const homeBtn = document.querySelector('.btn-home-js');
 
-homeBtn.addEventListener("click", onHomeBtnClick);
+homeBtn.addEventListener('click', onHomeBtnClick);
 
 function onHomeBtnClick(e) {
   e.preventDefault();
@@ -22,39 +22,39 @@ function onHomeBtnClick(e) {
 /////////////////////////////////// MyLibrary
 
 const refs = {
-  watched: document.querySelector(".btn-watched-js"),
-  queue: document.querySelector(".btn-queue-js"),
-  libraryBtn: document.querySelector(".btn-myLibrary-js"),
-  gallery: document.querySelector(".cards-gallery__list"),
+  watched: document.querySelector('.btn-watched-js'),
+  queue: document.querySelector('.btn-queue-js'),
+  libraryBtn: document.querySelector('.btn-myLibrary-js'),
+  gallery: document.querySelector('.cards-gallery__list'),
 };
 
 // const watched = JSON.parse(localStorage.getItem('watched'));
 // const queue = JSON.parse(localStorage.getItem('queue'));
 
-refs.watched.addEventListener("click", onClickWatchedFilms);
-refs.queue.addEventListener("click", onClickQueueFilms);
-refs.libraryBtn.addEventListener("click", onClickLibraryBtn);
+refs.watched.addEventListener('click', onClickWatchedFilms);
+refs.queue.addEventListener('click', onClickQueueFilms);
+refs.libraryBtn.addEventListener('click', onClickLibraryBtn);
 
 function onClickLibraryBtn(e) {
   apiService.resetPage();
   pagination.reset();
   onClickWatchedFilms();
-  refs.watched.classList.add("active");
-  refs.queue.classList.remove("active");
+  refs.watched.classList.add('active');
+  refs.queue.classList.remove('active');
 }
 
 function moviesLibraryMarkup() {
   clearFilmsGallery();
   const message =
     '<div class = "gallery-warning"><p>Sorry, you have not added any movies to the list. Return to the <a class = "gallery-warning__span" href="./index.html">HOME</a> </p><div>';
-  refs.gallery.insertAdjacentHTML("beforeend", message);
+  refs.gallery.insertAdjacentHTML('beforeend', message);
 }
 
 ////////// Watched Button
 function onClickWatchedFilms() {
-  refs.watched.classList.add("active");
-  refs.queue.classList.remove("active");
-  const watched = JSON.parse(localStorage.getItem("watched"));
+  refs.watched.classList.add('active');
+  refs.queue.classList.remove('active');
+  const watched = JSON.parse(localStorage.getItem('watched'));
   clearFilmsGallery();
   if (watched === null || watched.length === 0) {
     moviesLibraryMarkup();
@@ -66,10 +66,10 @@ function onClickWatchedFilms() {
 
 /////////////// Queue Button
 function onClickQueueFilms() {
-  refs.watched.classList.remove("active");
-  refs.queue.classList.add("active");
+  refs.watched.classList.remove('active');
+  refs.queue.classList.add('active');
 
-  const queue = JSON.parse(localStorage.getItem("queue"));
+  const queue = JSON.parse(localStorage.getItem('queue'));
   clearFilmsGallery();
   if (queue === null || queue.length === 0) {
     moviesLibraryMarkup();
@@ -88,5 +88,7 @@ function renderMovies(data) {
 
 ///////////////////// Clear container
 function clearFilmsGallery() {
-  refs.gallery.innerHTML = "";
+  refs.gallery.innerHTML = '';
 }
+
+export { renderMovies, clearFilmsGallery, moviesLibraryMarkup };
