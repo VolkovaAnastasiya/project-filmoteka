682d03ad0fd615155d78e8c71894fbb5ae58efeb
PiekharievaK <pechareva@gmail.com>
2022-01-22 06:45:33 +0200

    Добавляет модальное окно с разработчиками и стили к нему

--- src/js/dev-6/dev-6-main.js
+++ src/js/dev-6/dev-6-main.js
@@ -1,1 +1,82 @@
-console.log(`6`)
+import { devInfo } from "./developers-info";
+
+const footerModalOpen = document.querySelector('.copyright_modal-open-button');
+const teamModal = document.querySelector('.modal-project-developers');
+const devGalleryMarkup = renderModalDev(devInfo)
+
+
+const modalDevList = document.querySelector('.modal-project-developers__list');
+const devModalCloseBtn = document.querySelector('.dev-modal-close-button');
+
+console.log(teamModal)
+
+
+
+
+footerModalOpen.addEventListener('click',footerModalAction )
+
+function footerModalAction(evt) {
+  const devCardMore = document.querySelector('.modal-close-button__dev');
+  const modalMoreDev = document.querySelectorAll('.dev-card-btn');
+
+// Попытка обнулить открытые карточки, при закрытии модалки
+  // if (document.querySelectorAll('card-open-details')) {
+  //   document.querySelectorAll('card-open-details').classList = 'developer-card';
+  // }
+  // if (document.querySelectorAll('developer-card__comment is-open')) {
+  //   document.querySelectorAll('developer-card__comment is-open').classList.remove('is-open');
+  // }
+
+    
+  devModalCloseBtn.addEventListener('click',footerModalAction );
+  console.log(modalMoreDev);
+  modalMoreDev.forEach(elem => elem.addEventListener('click', onCardBtnClick))
+   
+  teamModal.classList.toggle('is-open');
+  document.body.classList.toggle('is-overflow');
+
+}
+
+
+function onCardBtnClick(evt) {
+  const devDataDiv = evt.currentTarget.parentNode;
+  evt.currentTarget.classList.toggle('button-active');
+  devDataDiv.querySelector('.developer-card__comment').classList.toggle('is-open');
+  console.log(document.querySelector('.developer-card__comment').parentElement);
+
+  if (devDataDiv.parentNode.classList == 'developer-card') {
+    devDataDiv.parentNode.classList = 'card-open-details';
+    return
+  }
+  if (devDataDiv.parentNode.classList == 'card-open-details') {
+    
+    devDataDiv.parentNode.classList = 'developer-card';
+    return
+  }
+ };
+
+function renderModalDev(dataArray) { 
+   
+  return dataArray.map(dev => {
+   
+    return  `<li class="developer-card">
+   <div>
+      <div alt= "photo of ${dev.name}" class="developer-card__photo dev-${dev.id}"></div>
+      <div class="developer-card__text-box" >
+      <h2 class="developer-card__title">${dev.name}</h2>
+      <p class="developer-card__dev-part">${dev.devPart}</p>
+      <p class="developer-card__comment ">${dev.devComment}</p>
+      </div>
+      <button class="dev-card-btn" dev-modal-close>
+      <svg class="dev-card-icon" viewBox="0 0 32 32" >
+      
+<path d="M22.086 20.914l2.829-2.829-8.914-8.914-8.914 8.914 2.828 2.828 6.086-6.086z"></path>
+      
+      </svg>
+      </button>
+      </div>`;
+    
+    }).join('');
+}
+
+modalDevList.innerHTML = devGalleryMarkup;
---
+++ src/js/dev-6/developers-info.js
@@ -0,0 +1,11 @@
+export const devInfo = [
+    {id: '1', name: 'Alex', devPart: 'description', devComment: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, labore? Non blanditiis aperiam odio est officiis nulla doloribus iure odit assumenda repudiandae? Ipsam, odio ducimus nihil voluptas aperiam fuga culpa?',  },
+  { id: '2', name: 'Serg', devPart: 'description', devComment: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, labore? Non blanditiis aperiam odio est officiis nulla doloribus iure odit assumenda repudiandae? Ipsam, odio ducimus nihil voluptas aperiam fuga culpa?', },
+  { id: '3', name: 'Katya', devPart: 'description', devComment: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, labore? Non blanditiis aperiam odio est officiis nulla doloribus iure odit assumenda repudiandae? Ipsam, odio ducimus nihil voluptas aperiam fuga culpa?', },
+  { id: '4', name: 'Nastya', devPart: 'description', devComment: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, labore? Non blanditiis aperiam odio est officiis nulla doloribus iure odit assumenda repudiandae? Ipsam, odio ducimus nihil voluptas aperiam fuga culpa?', },
+  { id: '5', name: 'Yura', devPart: 'description', devComment: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, labore? Non blanditiis aperiam odio est officiis nulla doloribus iure odit assumenda repudiandae? Ipsam, odio ducimus nihil voluptas aperiam fuga culpa?', },
+  { id: '6', name: 'Alex2', devPart: 'description', devComment: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, labore? Non blanditiis aperiam odio est officiis nulla doloribus iure odit assumenda repudiandae? Ipsam, odio ducimus nihil voluptas aperiam fuga culpa?', },
+  {id: '7', name: 'Kate', devPart: 'description', devComment: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, labore? Non blanditiis aperiam odio est officiis nulla doloribus iure odit assumenda repudiandae? Ipsam, odio ducimus nihil voluptas aperiam fuga culpa?',  },
+
+] 
+
