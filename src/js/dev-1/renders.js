48a7c2864bf69415bb220b5702696dd143c7a458
Yura Dolzhnikov <yuradolzhnikov@gmail.com>
2022-01-23 01:42:56 +0200

    Spinner

--- src/js/dev-1/renders.js
+++ src/js/dev-1/renders.js
@@ -1,24 +1,21 @@
-import movieTpl from '../../templates/film-card-main.hbs'
-import movieLibraryTpl from '../../templates/film-card-library.hbs'
+import movieTpl from '../../templates/film-card-main.hbs';
+import movieLibraryTpl from '../../templates/film-card-library.hbs';
+import { openSpinner, closeSpinner } from '../dev-5/spinner';
 
+const gallery = document.querySelector('.cards-gallery__list');
 
-
-const gallery = document.querySelector(".cards-gallery__list");
-
+openSpinner();
 // Для рендера популярных фильмов
 function renderTrends(data) {
-
-
   sessionStorage.clear();
-  
-  
   const markup = movieTpl(data);
   //console.log(data);
-
   gallery.innerHTML = markup;
 }
+setTimeout(closeSpinner, 500);
 
 // Рендер библиотеки
+
 function renderLibrary(data) {
   const markup = movieLibraryTpl(data);
 
