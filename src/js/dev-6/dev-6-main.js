c7b5a1456f70f6fa04c047a1dda753c2ab11a683
PiekharievaK <pechareva@gmail.com>
2022-01-25 03:08:06 +0200

    Улучшение функции действий с модальным окном
    1.Добавляет константу открытого модального окна, для улучшения читаемости,
    2. Добавляет проверку, для отрисовки карточек

--- src/js/dev-6/dev-6-main.js
+++ src/js/dev-6/dev-6-main.js
@@ -5,57 +5,57 @@ const footerModalOpen = document.querySelector('.copyright_modal-open-button');
 const teamModal = document.querySelector('.modal-project-developers');
 const modalDevList = document.querySelector('.modal-project-developers__list');
 const devModalCloseBtn = document.querySelector('.dev-modal-close-button');
-const mybutton = document.querySelector('.btn-To-Top')
+const mybutton = document.querySelector('.btn-To-Top');
 
 
-footerModalOpen.addEventListener('click',footerModalAction )
+footerModalOpen.addEventListener('click', footerModalAction);
 
 
 
 function footerModalAction() {
+  // Показывает и прячет модальное окно
   teamModal.classList.toggle('is-open');
   document.body.classList.toggle('is-overflow');
-  renderModalDev(devInfo);
+  const modalIsOpen = teamModal.classList.contains('is-open');
   
-    //  Вешает и снимает слушатель на кнопкe закрытия модального окна
-   toggleButtons(teamModal.classList.contains('is-open'), devModalCloseBtn, 'click', footerModalAction );
- 
+  // Рендер карточек только если модака открывается
+  if (modalIsOpen) {renderModalDev(devInfo)};
+  const modalMoreInfoBtn = document.querySelectorAll('.dev-card-btn');
 
-    //  Вешает и снимает слушатель на кнопках у карточек
-      const modalMoreInfoBtn = document.querySelectorAll('.dev-card-btn');
-      toggleButtonsforAll(teamModal.classList.contains('is-open'), modalMoreInfoBtn, 'click',  onCardBtnClick);
+   //  Вешает и снимает слушатель на кнопкe закрытия модального окна
+  toggleButtons(modalIsOpen, devModalCloseBtn, 'click', footerModalAction );
+ 
+   //  Вешает и снимает слушатель на кнопках у карточек
+  toggleButtonsforAll(modalIsOpen, modalMoreInfoBtn, 'click',  onCardBtnClick);
       
-      // Убирает унопку UP, при открытой модалке
-      if (teamModal.classList.contains('is-open')) {
+   // Убирает унопку UP, при открытой модалке
+      if (modalIsOpen) {
         mybutton.style.display = 'none';
       } else {
         mybutton.style.display = 'block';
-      }
-
+  };
 
       // Обнуляет открытые карточки послее закрытия модального окна.
       if (teamModal.querySelector('card-open-details')) {
-    teamModal.querySelectorAll('card-open-details').classList ='developer-card';
-    teamModal.querySelectorAll('developer-card__comment is-open').classList.remove('is-open');
-
+  teamModal.querySelectorAll('card-open-details').classList ='developer-card';
+  teamModal.querySelectorAll('developer-card__comment is-open').classList.remove('is-open');
   }
- 
-}
-    
+};
+
 
 function onCardBtnClick(evt) {
-  const devDataDiv = evt.currentTarget.parentNode;
+  const CardMoreBtn = evt.currentTarget.parentNode;
 
   evt.currentTarget.classList.toggle('button-active');
-  devDataDiv.querySelector('.developer-card__comment').classList.toggle('is-open');
+  CardMoreBtn.querySelector('.developer-card__comment').classList.toggle('is-open');
   
-  if (devDataDiv.parentNode.classList == 'developer-card') {
-    devDataDiv.parentNode.classList = 'card-open-details';
+  if (CardMoreBtn.parentNode.classList == 'developer-card') {
+    CardMoreBtn.parentNode.classList = 'card-open-details';
     return
   }
-  if (devDataDiv.parentNode.classList == 'card-open-details') {
+  if (CardMoreBtn.parentNode.classList == 'card-open-details') {
     
-    devDataDiv.parentNode.classList = 'developer-card';
+    CardMoreBtn.parentNode.classList = 'developer-card';
     return
   }
  };
@@ -63,7 +63,7 @@ function onCardBtnClick(evt) {
  function renderModalDev(dataArray) { 
    const cards = devCards(dataArray);
   modalDevList.innerHTML = cards;
- }
+};
  
  function toggleButtons(condition, target, event, foo) {    
    if (condition === true) {
